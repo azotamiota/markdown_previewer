cd82aea85cdadfe4e75a8ada179d4009f350f22c
azotamiota <azotamiota@gmail.com>
2022-02-03 17:19:42 +0000

    page operating, css in process

--- src/Preview.js
+++ src/Preview.js
@@ -2,8 +2,25 @@
 import ReactMarkdown from "react-markdown";
 
 const Preview = ({ content }) => {
-  console.log(content);
-  return <ReactMarkdown>{content}</ReactMarkdown>;
+  return (
+    <>
+      <div id="toolbar">
+        <label
+          id="preview-header"
+          for="preview"
+          className="d-flex ms-auto me-auto shadow p-2 rounded-top"
+        >
+          Preview
+        </label>
+      </div>
+      <div
+        id="preview"
+        className="d-flex flex-column ms-auto me-auto shadow rounded-buttom overflow-auto"
+      >
+        <ReactMarkdown>{content}</ReactMarkdown>
+      </div>
+    </>
+  );
 };
 
 export default Preview;
